70d63def16eec7555d240bdb41a61e0e197ca455
ugr-ccia-IA <100840948+ugr-ccia-IA@users.noreply.github.com>
2024-04-01 13:10:52 +0200

    Delete CMakeFiles directory

--- CMakeFiles/GLUILib.dir/src/glui/glui_bitmap_img_data.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/GLUILib.dir/src/glui/glui_bitmap_img_data.cpp.o: \
- /home/fgr/SoftwarePropio/GifHub_Versions/Cuenta_ugr-ccia-IA/practica2/src/glui/glui_bitmap_img_data.cpp \
- /usr/include/stdc-predef.h
